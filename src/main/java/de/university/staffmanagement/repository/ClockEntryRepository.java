c8cf2234d3d0aaab4d833736c0babff24627cc96
Tima <taltayev@hof-university.de>
2025-05-11 10:36:14 +0200

    added repository annotation

--- src/main/java/de/university/staffmanagement/repository/ClockEntryRepository.java
+++ src/main/java/de/university/staffmanagement/repository/ClockEntryRepository.java
@@ -3,10 +3,11 @@ package de.university.staffmanagement.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import de.university.staffmanagement.entity.ClockEntry;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface ClockEntryRepository  extends JpaRepository<ClockEntry, Long> {
 
     @Query("select c from ClockEntry c where c.clockOutTime IS NULL order by c.entryId asc")
--- src/main/java/de/university/staffmanagement/repository/LeaveRepository.java
+++ src/main/java/de/university/staffmanagement/repository/LeaveRepository.java
@@ -1,12 +1,13 @@
 package de.university.staffmanagement.repository;
 
 import de.university.staffmanagement.entity.LeaveRequest;
-import de.university.staffmanagement.entity.User;
 import de.university.staffmanagement.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LeaveRepository extends JpaRepository<LeaveRequest, Long>{
     List<LeaveRequest> findByStatus(Status status);
 }
